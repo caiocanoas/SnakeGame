be134fa9770bf428c675867ec76e365404658b0e
unknown <caio.canoas@gmail.com>
2020-07-10 16:31:36 -0300

    Movimento da Cobrinha

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let context = canvas.getContext("2d")
 let box = 32
 let snake = []
 snake[0] = {
-    x: 8 * box,
+    x: 1 * box,
     y: 8 * box
 }
+let direction = "right"
 
 function criarBG() {
     context.fillStyle = "red"
@@ -19,5 +20,27 @@ function criarCobra(){
     }
 }
 
-criarBG()
-criarCobra()
+function iniciarJogo(){
+    criarBG()
+    criarCobra()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == "right") snakeX += box
+    if(direction == "left") snakeX -= box
+    if(direction == "up") snakeY += box
+    if(direction == "down") snakeY -= box
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+
+}
+
+let jogo = setInterval(iniciarJogo , 100)
